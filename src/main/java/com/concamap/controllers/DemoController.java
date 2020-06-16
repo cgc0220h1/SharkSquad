af4ab9e816432e93b598e7a431e8bff11b84e137
Duc <vanduc2514@gmail.com>
2020-06-17 01:06:24 +0700

    Kiểm tra UTF-8 trong Thymeleaf

--- src/main/java/com/concamap/controllers/DemoController.java
+++ src/main/java/com/concamap/controllers/DemoController.java
@@ -3,7 +3,9 @@ package com.concamap.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 @RequestMapping("/")
@@ -12,7 +14,15 @@ public class DemoController {
     @GetMapping
     public ModelAndView showIndex() {
         ModelAndView modelAndView = new ModelAndView("index");
-        modelAndView.addObject("message","Hello World");
+        modelAndView.addObject("message","Xin chào");
+        return modelAndView;
+    }
+
+    @GetMapping("/form")
+    public ModelAndView redirect(@RequestParam("input") String input) {
+        System.out.println(input);
+        ModelAndView modelAndView = new ModelAndView("index");
+        modelAndView.addObject("result", input);
         return modelAndView;
     }
 }
