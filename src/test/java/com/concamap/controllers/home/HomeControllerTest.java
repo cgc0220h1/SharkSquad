30e5fbafac4d9950a8937f2926d24a5e3bdbd1e5
Duc <vanduc2514@gmail.com>
2020-06-22 17:09:14 +0700

    Merge remote-tracking branch 'origin/daiduong' into vanduc
    Thêm properties format sql
    Thêm test case

--- src/test/java/com/concamap/controllers/home/HomeControllerTest.java
+++ src/test/java/com/concamap/controllers/home/HomeControllerTest.java
@@ -9,17 +9,20 @@ import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT) //Tạo cổng mặc định
 class HomeControllerTest {
 
     private final WebApplicationContext webApplicationContext;
 
-    private MockMvc mockMvc; //Tạo ra url không cần phải localhost
+    MockMvc mockMvc; //Tạo ra url không cần phải localhost
 
     @InjectMocks
     private HomeController homeController;
@@ -34,16 +37,17 @@ class HomeControllerTest {
 
     @BeforeEach
     public void init() {
-        Post post = new Post();
-        post.setTitle("Dân tổ không sợ khổ");
-        postService.save(post);
-
-        MockitoAnnotations.initMocks(this);
+//        Post post = new Post();
+//        post.setTitle("Dân tổ không sợ khổ");
+//        postService.save(post);
+//
+//        MockitoAnnotations.initMocks(this);
         mockMvc = MockMvcBuilders.standaloneSetup(homeController).build();
     }
 
     @Test
-    public void whenGetRequest_thenReturnPost() {
+    public void whenGetRequest_thenReturnPost() throws Exception {
 //        mockMvc.perform() .andExpect(status().is(200)).andExpect(view().name("home/index"));
+        mockMvc.perform(MockMvcRequestBuilders.get("/")).andExpect(status().isOk()).andExpect(view().name("home/index"));
     }
 }
