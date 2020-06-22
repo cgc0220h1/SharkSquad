1a2cbd023c420dda955779d5d3d0cfa0f4b42da6
Vu Dai Duong <daiduongutc97@gmail.com>
2020-06-22 11:13:53 +0700

    Thêm ô search

--- src/main/java/com/concamap/controllers/home/PostController.java
+++ src/main/java/com/concamap/controllers/home/PostController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -26,4 +27,10 @@ public class PostController {
         modelAndView.addObject("post", postFound);
         return modelAndView;
     }
+
+//    @GetMapping("/user/{id}/create")
+//    public ModelAndView showCreatePost (@PathVariable("id") )
+//
+//    @PostMapping("/user/{id}/create")
+//    public ModelAndView create
 }
