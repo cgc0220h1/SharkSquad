7fcfe1f8e32f473625db5d428657b7f5dfbbbc6f
huyfr <61008601+huyfr@users.noreply.github.com>
2020-06-23 22:57:19 +0700

    Thêm api tìm tất cả comment của 1 bài viết

--- src/main/java/com/concamap/api/CommentAPI.java
+++ src/main/java/com/concamap/api/CommentAPI.java
@@ -1,30 +1,46 @@
 package com.concamap.api;
 
 import com.concamap.model.Comment;
+import com.concamap.model.Post;
 import com.concamap.services.comment.CommentService;
+import com.concamap.services.post.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
+@PropertySource("classpath:config/status.properties")
 @RequestMapping("/api/comments")
 public class CommentAPI {
 
+    @Value("${entity.exist}")
+    private int statusExist;
+
     private final CommentService commentService;
 
+    private final PostService postService;
+
     @Autowired
-    public CommentAPI(CommentService commentService) {
+    public CommentAPI(CommentService commentService, PostService postService) {
         this.commentService = commentService;
+        this.postService = postService;
     }
 
-    @RequestMapping(value = "/{}/create", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
+    @RequestMapping(value = "/{username}/create", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
-    public Comment createComment(@RequestBody Comment comment) {
+    public Comment createComment(@RequestBody Comment comment, @PathVariable String username) {
         return commentService.save(comment);
     }
 
+    @RequestMapping(value = "/{anchor-name}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseBody
+    public List<Comment> allComment(@PathVariable("anchor-name") String anchorName) {
+        Post postFound = postService.findExistByAnchorName(anchorName);
+        return commentService.findAllByPostAndStatus(postFound, statusExist);
+    }
 }
