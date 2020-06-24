4b7cdb16417825761112b389d713e0c583520c0f
huyfr <61008601+huyfr@users.noreply.github.com>
2020-06-24 22:53:46 +0700

    Thêm màn hình xem danh sách người dùng, thể loại

--- src/main/java/com/concamap/controllers/admin/AdminController.java
+++ src/main/java/com/concamap/controllers/admin/AdminController.java
@@ -1,7 +1,11 @@
 package com.concamap.controllers.admin;
 
+import com.concamap.model.Category;
 import com.concamap.model.Post;
+import com.concamap.model.Users;
+import com.concamap.services.category.CategoryService;
 import com.concamap.services.post.PostService;
+import com.concamap.services.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.data.domain.Page;
@@ -18,9 +22,15 @@ public class AdminController {
 
     private final PostService postService;
 
+    private final CategoryService categoryService;
+
+    private final UserService userService;
+
     @Autowired
-    public AdminController(PostService postService) {
+    public AdminController(PostService postService, CategoryService categoryService, UserService userService) {
         this.postService = postService;
+        this.categoryService = categoryService;
+        this.userService = userService;
     }
 
     @GetMapping("/overview")
@@ -29,6 +39,22 @@ public class AdminController {
         Page<Post> posts = postService.findAllExist(pageable);
         dashboard.addObject("allPost", posts);
         return dashboard;
+    }
+
+    @GetMapping("/categories")
+    ModelAndView loadCategories(Pageable pageable) {
+        ModelAndView categoriesView = new ModelAndView("admin/categories");
+        Page<Category> categories = categoryService.findAllExist(pageable);
+        categoriesView.addObject("categories", categories);
+        return categoriesView;
+    }
+
+    @GetMapping("/users")
+    ModelAndView loadUsers(Pageable pageable) {
+        ModelAndView usersView = new ModelAndView("admin/users");
+        Page<Users> users = userService.findAllExist(pageable);
+        usersView.addObject("users", users);
+        return usersView;
     }
 
     @ExceptionHandler(Exception.class)
