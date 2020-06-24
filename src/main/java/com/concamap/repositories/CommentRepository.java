969a7054adffc758fb6c88c34ba2aff7ad226175
Duc <vanduc2514@gmail.com>
2020-06-24 23:26:48 +0700

    1. Sửa giao diện css 2. Sử dụng jQuery lấy comment 3. Sử dụng jQuery post comment 4. Thay đổi phương thức save 5. Thêm phương thức ở API trả về page

--- src/main/java/com/concamap/repositories/CommentRepository.java
+++ src/main/java/com/concamap/repositories/CommentRepository.java
@@ -2,6 +2,8 @@ package com.concamap.repositories;
 
 import com.concamap.model.Comment;
 import com.concamap.model.Post;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 import java.util.List;
@@ -9,6 +11,8 @@ import java.util.List;
 public interface CommentRepository extends PagingAndSortingRepository<Comment, Integer> {
 
     List<Comment> findAllByPostAndStatus(Post post, int status);
+
+    Page<Comment> findAllByPostAndStatus(Post post, int statusExist, Pageable pageable);
 
     Comment findByIdAndStatus(int id, int status);
 }
