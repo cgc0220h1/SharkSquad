ba8a288d7e24bf9a4fc2d535eb67137bbfb89e2c
Vu Dai Duong <daiduongutc97@gmail.com>
2020-06-26 09:42:12 +0700

    Sửa chức năng CRUD category Thêm chức năng delete, view detail post cho Admin Thêm nút sửa xóa cho User

--- src/main/java/com/concamap/controllers/admin/AdminController.java
+++ src/main/java/com/concamap/controllers/admin/AdminController.java
@@ -1,9 +1,11 @@
 package com.concamap.controllers.admin;
 
 import com.concamap.model.Category;
+import com.concamap.model.Comment;
 import com.concamap.model.Post;
 import com.concamap.model.Users;
 import com.concamap.services.category.CategoryService;
+import com.concamap.services.comment.CommentService;
 import com.concamap.services.post.PostService;
 import com.concamap.services.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,11 +34,14 @@ public class AdminController {
 
     private final UserService userService;
 
+    private final CommentService commentService;
+
     @Autowired
-    public AdminController(PostService postService, CategoryService categoryService, UserService userService) {
+    public AdminController(PostService postService, CategoryService categoryService, UserService userService, CommentService commentService) {
         this.postService = postService;
         this.categoryService = categoryService;
         this.userService = userService;
+        this.commentService = commentService;
     }
 
     private String removeAccent(String s) {
@@ -46,7 +51,7 @@ public class AdminController {
     }
 
     @GetMapping("/overview")
-    ModelAndView loadDashboard(Pageable pageable) throws Exception {
+    public ModelAndView loadDashboard(Pageable pageable) throws Exception {
         ModelAndView dashboard = new ModelAndView("admin/dashboard");
         Page<Post> posts = postService.findAllExist(pageable);
         dashboard.addObject("allPost", posts);
@@ -54,7 +59,7 @@ public class AdminController {
     }
 
     @GetMapping("/categories")
-    ModelAndView loadCategories(Pageable pageable) {
+    public ModelAndView loadCategories(Pageable pageable) {
         ModelAndView categoriesView = new ModelAndView("admin/categories");
         Page<Category> categories = categoryService.findAllExist(pageable);
         categoriesView.addObject("categories", categories);
@@ -72,11 +77,14 @@ public class AdminController {
     @PostMapping("/categories/edit")
     public RedirectView updateCategory(@ModelAttribute("category") Category category) {
         Category categoryFound = categoryService.findExistById(category.getId());
+//        http://localhost:8888/admin/categories/suc-khoe/edit
         long now = System.currentTimeMillis();
+        categoryFound.setDescription(category.getDescription());
+        categoryFound.setTitle(category.getTitle());
         categoryFound.setUpdatedDate(new Timestamp(now));
         categoryFound.setAnchorName(removeAccent(category.getDescription()));
         categoryService.save(categoryFound);
-        return new RedirectView("/categories");
+        return new RedirectView("/admin/categories");
     }
 
     @GetMapping("/categories/create")
@@ -90,19 +98,49 @@ public class AdminController {
     @PostMapping("/categories/create")
     public RedirectView createCategory(@ModelAttribute("category") Category category) {
         long now = System.currentTimeMillis();
+        category.setStatus(1);
         category.setUpdatedDate(new Timestamp(now));
         category.setCreatedDate(new Timestamp(now));
         category.setAnchorName(removeAccent(category.getDescription()));
         categoryService.save(category);
-        return new RedirectView("/categories");
+        return new RedirectView("/admin/categories");
     }
 
     @GetMapping("/categories/{anchor-name}/delete")
     public RedirectView deleteCategory(@PathVariable("anchor-name") String anchorName) {
         Category category = categoryService.findByAnchorName(anchorName);
         category.setUpdatedDate(new Timestamp(System.currentTimeMillis()));
+        category.setStatus(0);
         categoryService.save(category);
-        return new RedirectView("/categories");
+        return new RedirectView("/admin/categories");
+    }
+
+    @GetMapping("/users/posts/{anchor-name}/delete")
+    public RedirectView adminDeletePost(@PathVariable("anchor-name") String anchorName){
+        Post post = postService.findExistByAnchorName(anchorName);
+        post.setStatus(0);
+        post.setUpdatedDate(new Timestamp(System.currentTimeMillis()));
+        postService.save(post);
+        return new RedirectView("/admin/overview");
+    }
+
+    @GetMapping("/users/{username}/posts/{anchor-name}")
+    public ModelAndView adminViewPost(@PathVariable("anchor-name") String anchorName,
+                                      @PathVariable("username") String username,
+                                      @SessionAttribute("recentPostList") List<Post> recentPosts,
+                                      @SessionAttribute("randomPostList") List<Post> randomPosts,
+                                      @SessionAttribute("categoryList") List<Category> categoryList){
+        ModelAndView modelAndView = new ModelAndView("admin/viewDetailPost");
+        Post postFound = postService.findExistByAnchorName(anchorName);
+
+        List<Comment> allComment = commentService.findAllExistByPost(postFound);
+
+        modelAndView.addObject("post", postFound);
+        modelAndView.addObject("allComment", allComment);
+        modelAndView.addObject("recentPostList", recentPosts);
+        modelAndView.addObject("randomPostList", randomPosts);
+        modelAndView.addObject("categoryList", categoryList);
+        return modelAndView;
     }
 
     @GetMapping("/users")
