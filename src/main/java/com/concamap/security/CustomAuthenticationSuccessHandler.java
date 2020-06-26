9be1df7e9ae49e40adb369dccca69e783c792289
huyfr <61008601+huyfr@users.noreply.github.com>
2020-06-26 13:27:48 +0700

    Thêm điều hướng dựa theo roles

---
+++ src/main/java/com/concamap/security/CustomAuthenticationSuccessHandler.java
@@ -0,0 +1,35 @@
+package com.concamap.security;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Service;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Set;
+
+@Service
+public class CustomAuthenticationSuccessHandler implements AuthenticationSuccessHandler {
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
+
+    }
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+        Set<String> roles = AuthorityUtils.authorityListToSet(authentication.getAuthorities());
+
+        if  (roles.contains("USER")) {
+            response.sendRedirect("/");
+        } else if (roles.contains("ROLE_ADMIN")) {
+            response.sendRedirect("/admin/overview");
+        } else {
+            response.sendRedirect("/login");
+        }
+    }
+}
